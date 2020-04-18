fe1e354244c0d77fb18bd121b55575b7feb11f73
dlkybc <1341601069@qq.com.com>
2020-04-18 16:09:06 +0800

    新建登录分支  新建登录组件

--- router/index.js
+++ router/index.js
@@ -1,17 +1,20 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Login from '../components/login/login.vue'
+//  @->src
+import Login from "@/components/login/login.vue";
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
-  const routes = [
-		{
-			path: "/",
-		
-		},
-	];
+const routes = [
+	{
+		path: "/login",
+		name: 'login',
+		component: Login,
+	},
+];
 
 const router = new VueRouter({
-  routes
-})
+	routes,
+});
 
-export default router
+export default router;
