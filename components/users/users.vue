54650222b9b89a67ac9b4123cf89d185a3eff379
dlkybc <1341601069@qq.com.com>
2020-04-19 17:56:23 +0800

    create_time特殊使用

--- components/users/users.vue
+++ components/users/users.vue
@@ -13,18 +13,33 @@
 				<el-input placeholder="请输入内容" v-model="query" class="inputSearch">
 					<el-button slot="append" icon="el-icon-search"></el-button>
 				</el-input>
-				<el-button type="success">添加用户</el-button>
+				<el-button type="success">添加用户{{ date1 | fmtdate }}</el-button>
 			</el-col>
 		</el-row>
 		<!-- 表格 -->
-		<el-table :data="tableData" style="width: 100%">
+		<el-table :data="userList" style="width: 100%">
 			<el-table-column label="#" width="60" type="index"> </el-table-column>
 			<el-table-column prop="name" label="姓名" width="80"> </el-table-column>
 			<el-table-column prop="address" label="邮箱"> </el-table-column>
-			<el-table-column prop="address" label="电话"> </el-table-column>
-			<el-table-column prop="address" label="创建时间"> </el-table-column>
-			<el-table-column prop="address" label="用户状态"> </el-table-column>
-			<el-table-column prop="address" label="操作"> </el-table-column>
+			<el-table-column prop="phone" label="电话"> </el-table-column>
+			<el-table-column label="创建时间">
+				<!-- 如果单元格内显示的内容不是字符串(文本)，需要给被显示的内容外层包裹一层template -->
+				<!--
+          template内部要用数据 设置slot-scope属性
+slot-scope会自动去找上一级数据源，肯定会找到userlist ,所以写成slot-scope="aaa"  	{{ aaa.row.create_time | fmtdate }}也可以，会把scope会自动去找数据源，肯定会找到userlist赋值给aaa，通常取名scope
+          该属性的值是要用数据(create_time)的数据源userlist
+         -->
+				<!--
+           slot-scope的值userlist其实就是el-table绑定的数据userlist
+           userlist.row指的是数组中的每个对象
+          -->
+				<template slot-scope="scope">
+					{{ scope.row.create_time | fmtdate }}
+				</template>
+				<!-- 不用写prop -->
+			</el-table-column>
+			<el-table-column prop="status" label="用户状态"> </el-table-column>
+			<el-table-column prop="done" label="操作"> </el-table-column>
 		</el-table>
 		<!-- 分页 -->
 	</el-card>
@@ -33,42 +48,63 @@
 export default {
 	data() {
 		return {
+			date1: 1587287907825,
 			query: "",
-			tableData: [
+			userList: [
 				{
 					date: "2016-05-02",
 					name: "王小虎",
-					address: "上海市普陀区金沙江路 1518 弄",
+					address: "1341601000@qq.com",
+					create_time: new Date().getTime(),
+					phone: 18722220000,
+					status: "false",
+					done: "操作",
 				},
 				{
 					date: "2016-05-04",
 					name: "王小虎",
-					address: "上海市普陀区金沙江路 1517 弄",
+					address: "1341601000@qq.com",
+					create_time: new Date().getTime(),
+					phone: 18722220000,
+					status: "false",
+					done: "操作",
 				},
 				{
 					date: "2016-05-01",
 					name: "王小虎",
-					address: "上海市普陀区金沙江路 1519 弄",
+					address: "1341601000@qq.com",
+					create_time: new Date().getTime(),
+					phone: 18722220000,
+					status: "false",
+					done: "操作",
 				},
 				{
 					date: "2016-05-03",
 					name: "王小虎",
-					address: "上海市普陀区金沙江路 1516 弄",
+					address: "1341601000@qq.com",
+					create_time: new Date().getTime(),
+					phone: 18722220000,
+					status: "false",
+					done: "操作",
 				},
-      ],
-      pagenum:1,
-      pagesize:2
+			],
+			pagenum: 1,
+			pagesize: 2,
+			//分页
+			total: 0,
 		};
-  },
-  created() {
-    this.getUserList()
-  },
+	},
+	created() {
+		this.getUserList();
+	},
 	methods: {
-    //获取用户列表的请求
-    //需要授权的api 必须在请求头中使用 Authorization 字段提供 token令牌
+		//获取用户列表的请求
+		//需要授权的api 必须在请求头中使用 Authorization 字段提供 token令牌
 		async getUserList() {
-    const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
-    },
+			const res = await this.$http.get(
+				`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
+			);
+		},
 	},
 };
 </script>
--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 import Vuex from "vuex";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-import '@/assets/css/reset.css'
-import MyHttpServer from '@/plugins/http.js'
-
+import "@/assets/css/reset.css";
+import MyHttpServer from "@/plugins/http.js";
+import moment from 'moment'
 //回顾axios
 // import axios from 'axios'
 // Vue.prototype.$http=axios
 //Vue.use()是vue插件的用法  axios不是vue插件
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(ElementUI, Vuex);
 //Vue.use(ElementUI, Vuex, MyHttpServer);  这样写MyHttpServer不生效 需要单独写一个use()
 Vue.use(MyHttpServer);
 
-
+//全局过滤器
+Vue.filter("fmtdate", (v) => {
+	//npm i moment
+	return moment(v).format("YYYY-MM-DD");
+});
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount("#app");
