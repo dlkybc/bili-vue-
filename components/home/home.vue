555c1c4a790a1ec7fa0cfeab1919dd3ebd4b88a5
dlkybc <1341601069@qq.com.com>
2020-04-19 15:14:11 +0800

    头部样式调整

--- components/home/home.vue
+++ components/home/home.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>
-home
-  </div>
+	<el-container class="container">
+		<el-header class="header">
+			<el-row style="height:100%;">
+				<el-col :span="4" style="height:100%;">
+					<div class="grid-content bg-purple" style="height:100%;">
+						<img src="../../assets/logo.png" alt="无法显示图片" style="height:100%;width:100%;"/>
+					</div>
+				</el-col>
+				<el-col :span="18" class="middle">电商后台管理系统 </el-col>
+				<el-col :span="2"
+					><div class="grid-content bg-purple">
+						<a href="" class="loginout">退出</a>
+					</div></el-col
+				>
+			</el-row>
+		</el-header>
+		<el-container>
+			<el-aside width="200px" class="aside">Aside</el-aside>
+			<el-main class="main">Main</el-main>
+		</el-container>
+	</el-container>
 </template>
 <script>
-export default {
-  
-}
+export default {};
 </script>
 <style lang="scss">
-
+.container {
+	height: 100%;
+	.header {
+		background: #b3c0d1;
+    .middle{
+   text-align: center;
+   line-height: 60px;
+    }
+    .loginout{
+      text-decoration: none;
+        line-height: 60px;
+    }
+	}
+	.aside {
+		background: #d3dce6;
+	}
+	.main {
+		background: #e9eef3;
+	}
+}
 </style>
