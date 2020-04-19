2daa9689143d0f44032e374963100b2d47c68aec
dlkybc <1341601069@qq.com.com>
2020-04-19 16:50:28 +0800

    设置请求头

--- components/users/users.vue
+++ components/users/users.vue
@@ -1,13 +1,85 @@
 <template>
-  <div>
- 用户
-  </div>
+	<!-- 小容器el-card -->
+	<el-card class="box-card">
+		<!-- 面包屑 -->
+		<el-breadcrumb separator-class="el-icon-arrow-right">
+			<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+			<el-breadcrumb-item>用户管理</el-breadcrumb-item>
+			<el-breadcrumb-item>用户列表</el-breadcrumb-item>
+		</el-breadcrumb>
+		<!-- 搜索 -->
+		<el-row class="searchRow">
+			<el-col>
+				<el-input placeholder="请输入内容" v-model="query" class="inputSearch">
+					<el-button slot="append" icon="el-icon-search"></el-button>
+				</el-input>
+				<el-button type="success">添加用户</el-button>
+			</el-col>
+		</el-row>
+		<!-- 表格 -->
+		<el-table :data="tableData" style="width: 100%">
+			<el-table-column label="#" width="60" type="index"> </el-table-column>
+			<el-table-column prop="name" label="姓名" width="80"> </el-table-column>
+			<el-table-column prop="address" label="邮箱"> </el-table-column>
+			<el-table-column prop="address" label="电话"> </el-table-column>
+			<el-table-column prop="address" label="创建时间"> </el-table-column>
+			<el-table-column prop="address" label="用户状态"> </el-table-column>
+			<el-table-column prop="address" label="操作"> </el-table-column>
+		</el-table>
+		<!-- 分页 -->
+	</el-card>
 </template>
 <script>
 export default {
-
-}
+	data() {
+		return {
+			query: "",
+			tableData: [
+				{
+					date: "2016-05-02",
+					name: "王小虎",
+					address: "上海市普陀区金沙江路 1518 弄",
+				},
+				{
+					date: "2016-05-04",
+					name: "王小虎",
+					address: "上海市普陀区金沙江路 1517 弄",
+				},
+				{
+					date: "2016-05-01",
+					name: "王小虎",
+					address: "上海市普陀区金沙江路 1519 弄",
+				},
+				{
+					date: "2016-05-03",
+					name: "王小虎",
+					address: "上海市普陀区金沙江路 1516 弄",
+				},
+      ],
+      pagenum:1,
+      pagesize:2
+		};
+  },
+  created() {
+    this.getUserList()
+  },
+	methods: {
+    //获取用户列表的请求
+    //需要授权的api 必须在请求头中使用 Authorization 字段提供 token令牌
+		async getUserList() {
+    const res = await this.$http.get(`users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`)
+    },
+	},
+};
 </script>
 <style lang="scss">
-
+.box-card {
+	height: 100%;
+	.searchRow {
+		margin-top: 25px;
+		.inputSearch {
+			width: 300px;
+		}
+	}
+}
 </style>
